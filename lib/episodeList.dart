950e617d8980e8706d8aa90adc7d5568788d6666
Omkarchavan28 <chavanomkar245@gmail.com>
2019-12-13 20:23:03 +0530

    code cleaned

--- lib/episodeList.dart
+++ lib/episodeList.dart
@@ -1,61 +1,45 @@
-import 'dart:convert';
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:lacasadepapel/services/services.dart';
-import 'package:http/http.dart' as http;
 
 class EpisodeList extends StatefulWidget {
+  final data;
+
+  const EpisodeList({Key key, this.data}) : super(key: key);
+
   @override
   _EpisodeListState createState() => _EpisodeListState();
 }
 
 class _EpisodeListState extends State<EpisodeList> {
-  var data;
-  @override
-  void initState() {
-    super.initState();
-    getData();
-  }
-
-  Future getData() async {
-    print('getting');
-    print(data);
-    http.Response res = await http.get(
-        'http://api.tvmaze.com/singlesearch/shows?q=money%20heist&embed=episodes');
-    data = jsonDecode(res.body);
-    setState(() {
-      print('got');
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: ListView.builder(
-        itemCount: data['_embedded']['episodes'].length,
-        itemBuilder: (BuildContext context, int index) {
-          return data['_embedded']['episodes'][index]['season'] == 4
-              ? null
-              : Card(
-                  color: Colors.red[600],
-                  child: Container(
-                    // height: 500,
-                    child: Column(
+    return ListView.builder(
+      itemCount: widget.data['_embedded']['episodes'].length,
+      itemBuilder: (BuildContext context, int index) {
+        return widget.data['_embedded']['episodes'][index]['season'] == 4
+            ? null
+            : Card(
+                color: Colors.red[600],
+                child: Column(
+                  children: <Widget>[
+                    Column(
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: <Widget>[
                         Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: <Widget>[
                             Text(
-                              data['_embedded']['episodes'][index]['name'],
+                              widget.data['_embedded']['episodes'][index]
+                                  ['name'],
                               style: TextStyle(
                                 fontSize: 20.0,
                                 color: Colors.white,
                               ),
                             ),
                             Text(
-                              data['_embedded']['episodes'][index]['season']
+                              widget.data['_embedded']['episodes'][index]
+                                      ['season']
                                   .toString(),
                               style: TextStyle(
                                 fontSize: 20.0,
@@ -67,7 +51,8 @@ class _EpisodeListState extends State<EpisodeList> {
                         Align(
                           alignment: Alignment.topRight,
                           child: Text(
-                            data['_embedded']['episodes'][index]['number']
+                            widget.data['_embedded']['episodes'][index]
+                                    ['number']
                                 .toString(),
                             style: TextStyle(
                               fontSize: 20.0,
@@ -76,11 +61,12 @@ class _EpisodeListState extends State<EpisodeList> {
                           ),
                         ),
                         Image(
-                            image: CachedNetworkImageProvider(data['_embedded']
-                                ['episodes'][index]['image']['original'])),
+                            image: CachedNetworkImageProvider(
+                                widget.data['_embedded']['episodes'][index]
+                                    ['image']['original'])),
                         Text(
-                          removeAllHtmlTags(
-                              data['_embedded']['episodes'][index]['summary']),
+                          removeAllHtmlTags(widget.data['_embedded']['episodes']
+                              [index]['summary']),
                           style: TextStyle(
                             fontSize: 14.0,
                             color: Colors.white,
@@ -88,10 +74,10 @@ class _EpisodeListState extends State<EpisodeList> {
                         ),
                       ],
                     ),
-                  ),
-                );
-        },
-      ),
+                  ],
+                ),
+              );
+      },
     );
   }
 }
